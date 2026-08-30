2145d858667fc52cd6d9072aeee96e736bb73b73
Peter Cudmore <peter.cudmore@uqconnect.edu.au>
2026-08-30 20:33:06 +1000

    Move Coker backend tests to runtime repository

--- tests/backends/common/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def coker_backend():
-    import coker_backend
-    from coker.backends.backend import get_backend_by_name
-
-    return get_backend_by_name("coker", set_current=False)
