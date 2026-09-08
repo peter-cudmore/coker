1206962ecf0a18969a14ec4135da34bc7fec6e58
Peter Cudmore <peter.cudmore@uqconnect.edu.au>
2026-09-08 10:53:13 +1000

    refactor: route trajectory accessors through evaluation

--- tests/dynamical_systems/test_variational_problem_builder.py
+++ tests/dynamical_systems/test_variational_problem_builder.py
@@ -154,6 +154,36 @@ def test_private_trajectory_spaces_preserve_function_evaluation_dimensions():
         assert np.asarray(value) == pytest.approx([3.0])
 
 
+def test_state_accessor_emits_function_evaluation_expression():
+    system = make_parameterised_integrator()
+    with VariationalProblemBuilder(system, t_final=1.0) as builder:
+        for time in (builder.t, 0, builder.t_final):
+            expression = builder.state(time)
+            _op, evaluated, _marker = builder._trace.nodes[
+                builder._trace.nodes[expression.index][1].index
+            ]
+            assert _op.name == "EVALUATE"
+            assert evaluated.index == builder._state.index
+
+
+def test_function_valued_input_accessor_emits_function_evaluation_expression():
+    system = make_parameterised_integrator()
+    with VariationalProblemBuilder(system, t_final=1.0) as builder:
+        expression = builder.input(builder.t)
+        _op, evaluated, _time = builder._trace.nodes[
+            builder._trace.nodes[expression.index][1].index
+        ]
+        assert _op.name == "EVALUATE"
+        assert evaluated.index == builder._input.index
+
+
+def test_state_accessor_evaluation_preserves_vector_shape():
+    system = make_parameterised_integrator()
+    with VariationalProblemBuilder(system, t_final=1.0) as builder:
+        state = builder.state(builder.t_final)
+        assert state.shape == (1,)
+
+
 def test_output_supports_path_constraints_and_terminal_objectives():
     system = make_parameterised_integrator()
     with VariationalProblemBuilder(system, t_final=1.0) as builder:
